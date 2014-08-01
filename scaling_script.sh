aed8fd0040ba4cd01fdddaa23279f98ad7fa3a36
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2014-08-01 17:05:20 -0400

    add Esperanto flag

--- scaling_script.sh
+++ scaling_script.sh
@@ -1,18 +1,6 @@
-convert pirate.svg -scale 32x32 pirate.png
-convert pirate.png -background none -gravity center -extent 32x32 pirate32.png
-convert pirate.svg -scale 16x16 pirate.png
-convert pirate.png -background none -gravity center -extent 16x16 pirate16.png
-rm pirate.png
-
-convert UFP.svg -scale 32x32 UFP.png
-convert UFP.png -background none -gravity center -extent 32x32 UFP32.png
-convert UFP.svg -scale 16x16 UFP.png
-convert UFP.png -background none -gravity center -extent 16x16 UFP16.png
-rm UFP.png
-
-convert Klingon.svg -scale 32x32 Klingon.png
-convert Klingon.png -background none -gravity center -extent 32x32 Klingon32.png
-convert Klingon.svg -scale 16x16 Klingon.png
-convert Klingon.png -background none -gravity center -extent 16x16 Klingon16.png
-rm Klingon.png
+convert Canary.png -scale 32x32 Canary2.png
+convert Canary2.png -background none -gravity center -extent 32x32 Canary32.png
+convert Canary.png -scale 16x16 Canary2.png
+convert Canary2.png -background none -gravity center -extent 16x16 Canary16.png
+rm Canary2.png
 

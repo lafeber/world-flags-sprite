a2a6dc04bc2e360b584b64c44a4eaa8de31294af
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2014-08-01 17:44:44 -0400

    some missing flags

--- scaling_script.sh
+++ scaling_script.sh
@@ -1,6 +1,7 @@
-convert Canary.png -scale 32x32 Canary2.png
-convert Canary2.png -background none -gravity center -extent 32x32 Canary32.png
-convert Canary.png -scale 16x16 Canary2.png
-convert Canary2.png -background none -gravity center -extent 16x16 Canary16.png
-rm Canary2.png
-
+for i in $(basename -s .png *.png); do
+  convert ${i}.png -scale 32x32 ${i}2.png
+  convert ${i}2.png -background none -gravity center -extent 32x32 ${i}32.png
+  convert ${i}.png -scale 16x16 ${i}2.png
+  convert ${i}2.png -background none -gravity center -extent 16x16 ${i}16.png
+  rm ${i}2.png
+done

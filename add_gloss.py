3c79b25e34eb0cdcd0faea74f10439eedd04bb9e
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2014-08-08 09:44:08 -0400

    gloss-adding python program

---
+++ add_gloss.py
@@ -0,0 +1,15 @@
+import sys
+import imread
+import numpy as np
+
+for f in sys.argv[1:]:
+    print f
+    im = imread.imread(f)[:, :, :3]
+    ramp = np.zeros(im.shape)
+    ramp[...] = 0.5
+    half = ramp.shape[0] / 2
+    ramp[:half, :, :] = np.linspace(0.7, 0.5, half).reshape((half, 1, 1))
+    new = 1.0 - 2 * (1 - ramp) * (1 - im.astype(float) / 255.0)
+    new = new * 255
+    new = new.astype(np.uint8)
+    imread.imsave("bumped_" + f, new)
